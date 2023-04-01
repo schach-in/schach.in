514ce75b02723cb1cb188609020d9b0975206bea
Gustaf Mossakowski <gustaf@koenige.org>
2023-04-01 14:15:56 +0200

    main.php: do not set paths here, just include main zzwrap script

--- www/_scripts/main.php
+++ www/_scripts/main.php
@@ -1,18 +1,8 @@
 <?php 
 
 // zzwrap (Zugzwang Project)
-// Copyright (c) 2007-2012 Gustaf Mossakowski <gustaf@koenige.org>
+// Copyright (c) 2007-2012, 2023 Gustaf Mossakowski <gustaf@koenige.org>
 // Main script
 
-// root directory
-// some providers are not able to configure the root directory correctly
-// then you'll have to correct that here
-$zz_conf['root'] = $_SERVER['DOCUMENT_ROOT'];
-// scripts library
-// if your provider does not support putting the include scripts below
-// document root, change accordingly
-$zz_setting['cms_dir'] = realpath($zz_conf['root'].'/..');
-
-// CMS will be started
-require_once $zz_setting['cms_dir'].'/_inc/modules/zzwrap/zzwrap/zzwrap.php';
+require_once $_SERVER['DOCUMENT_ROOT'].'/../_inc/modules/zzwrap/zzwrap/zzwrap.php';
 zzwrap();
